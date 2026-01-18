579c8d076c5c99b1129d54fb3ca3ea19971f2c5a
YOUICHI AKASHI <akashi@next-location.com>
2026-01-18 20:15:35 +0900

    feat: show all equipment on focus, filter as user types

--- app/(authenticated)/equipment/movement/EquipmentMovementForm.tsx
+++ app/(authenticated)/equipment/movement/EquipmentMovementForm.tsx
@@ -381,7 +381,7 @@ export default function EquipmentMovementForm({
             <input type="hidden" name="equipment_id" value={formData.equipment_id} required />
 
             {/* 検索結果ドロップダウン */}
-            {showEquipmentList && equipmentSearch && (
+            {showEquipmentList && (
               <div className="absolute z-10 w-full mt-1 bg-white border border-gray-300 rounded-md shadow-lg max-h-60 overflow-y-auto">
                 {filteredEquipment.length > 0 ? (
                   filteredEquipment.map((equip) => (
